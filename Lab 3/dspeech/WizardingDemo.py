7b2d22e17d1d7a77f392658f5118c2fb4c657885
Caitlin Stanton <caitlinstanton44@gmail.com>
2021-03-20 18:19:08 -0400

    bill

--- Lab 3/dspeech/WizardingDemo.py
+++ Lab 3/dspeech/WizardingDemo.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import io
+import os
+import shutil
+from subprocess import Popen, PIPE
+from string import Template
+from struct import Struct
+from threading import Thread
+from time import sleep, time
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from wsgiref.simple_server import make_server
+
+import picamera
+from ws4py.websocket import WebSocket
+from ws4py.server.wsgirefserver import (
+    WSGIServer,
+    WebSocketWSGIHandler,
+    WebSocketWSGIRequestHandler,
+)
+from ws4py.server.wsgiutils import WebSocketWSGIApplication
+
 import time, logging
 from datetime import datetime
 import threading, collections, queue, os, os.path
@@ -10,6 +33,132 @@ from halo import Halo
 from scipy import signal
 
 logging.basicConfig(level=20)
+
+# from flask import Flask, Response,render_template
+# from flask_socketio import SocketIO, send, emit
+# import board
+# import busio
+# import socket
+
+# i2c = busio.I2C(board.SCL, board.SDA)
+
+# hostname = socket.gethostname()
+# hardware = 'plughw:2,0'
+
+# app = Flask(__name__)
+# socketio = SocketIO(app)
+# audio_stream = Popen("/usr/bin/cvlc alsa://"+hardware+" --sout='#transcode{vcodec=none,acodec=mp3,ab=256,channels=2,samplerate=44100,scodec=none}:http{mux=mp3,dst=:8080/}' --no-sout-all --sout-keep", shell=True)
+
+# @socketio.on('speak')
+# def handel_speak(val):
+#     call(f"espeak '{val}'", shell=True)
+
+###########################################
+# CONFIGURATION
+WIDTH = 640
+HEIGHT = 480
+FRAMERATE = 24
+HTTP_PORT = 8082
+WS_PORT = 8084
+COLOR = u'#444'
+BGCOLOR = u'#333'
+JSMPEG_MAGIC = b'jsmp'
+JSMPEG_HEADER = Struct('>4sHH')
+VFLIP = False
+HFLIP = False
+
+###########################################
+
+
+class StreamingHttpHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.do_GET()
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+            return
+        elif self.path == '/jsmpg.js':
+            content_type = 'application/javascript'
+            content = self.server.jsmpg_content
+        elif self.path == '/index.html':
+            content_type = 'text/html; charset=utf-8'
+            tpl = Template(self.server.index_template)
+            content = tpl.safe_substitute(dict(
+                WS_PORT=WS_PORT, WIDTH=WIDTH, HEIGHT=HEIGHT, COLOR=COLOR,
+                BGCOLOR=BGCOLOR))
+        else:
+            self.send_error(404, 'File not found')
+            return
+        content = content.encode('utf-8')
+        self.send_response(200)
+        self.send_header('Content-Type', content_type)
+        self.send_header('Content-Length', len(content))
+        #self.send_header('Last-Modified', self.date_time_string(time()))
+        self.end_headers()
+        if self.command == 'GET':
+            self.wfile.write(content)
+
+
+class StreamingHttpServer(HTTPServer):
+    def __init__(self):
+        super(StreamingHttpServer, self).__init__(
+                ('', HTTP_PORT), StreamingHttpHandler)
+        with io.open('index.html', 'r') as f:
+            self.index_template = f.read()
+        with io.open('jsmpg.js', 'r') as f:
+            self.jsmpg_content = f.read()
+
+
+class StreamingWebSocket(WebSocket):
+    def opened(self):
+        self.send(JSMPEG_HEADER.pack(JSMPEG_MAGIC, WIDTH, HEIGHT), binary=True)
+
+
+class BroadcastOutput(object):
+    def __init__(self, camera):
+        print('Spawning background conversion process')
+        self.converter = Popen([
+            'ffmpeg',
+            '-f', 'rawvideo',
+            '-pix_fmt', 'yuv420p',
+            '-s', '%dx%d' % camera.resolution,
+            '-r', str(float(camera.framerate)),
+            '-i', '-',
+            '-f', 'mpeg1video',
+            '-b', '800k',
+            '-r', str(float(camera.framerate)),
+            '-'],
+            stdin=PIPE, stdout=PIPE, stderr=io.open(os.devnull, 'wb'),
+            shell=False, close_fds=True)
+
+    def write(self, b):
+        self.converter.stdin.write(b)
+
+    def flush(self):
+        print('Waiting for background conversion process to exit')
+        self.converter.stdin.close()
+        self.converter.wait()
+
+
+class BroadcastThread(Thread):
+    def __init__(self, converter, websocket_server):
+        super(BroadcastThread, self).__init__()
+        self.converter = converter
+        self.websocket_server = websocket_server
+
+    def run(self):
+        try:
+            while True:
+                buf = self.converter.stdout.read1(32768)
+                if buf:
+                    self.websocket_server.manager.broadcast(buf, binary=True)
+                elif self.converter.poll() is not None:
+                    break
+        finally:
+            self.converter.stdout.close()
 
 class Audio(object):
     """Streams raw audio from microphone. Data is received in a separate thread, and stored in a buffer, to be read from."""
@@ -151,9 +300,8 @@ class VADAudio(Audio):
                     yield None
                     ring_buffer.clear()
 
-
 def main(ARGS):
-    # Load DeepSpeech model
+
     if os.path.isdir(ARGS.model):
         model_dir = ARGS.model
         ARGS.model = os.path.join(model_dir, 'output_graph.pb')
@@ -193,11 +341,63 @@ def main(ARGS):
                 vad_audio.write_wav(os.path.join(ARGS.savewav, datetime.now().strftime("savewav_%Y-%m-%d_%H-%M-%S_%f.wav")), wav_data)
                 wav_data = bytearray()
             text = stream_context.finishStream()
-            if (text == "bill" or text == "work"):
-                print("Recognized: %s" % text)
+            if (text == "bill"):
+                print('Initializing camera')
+                with picamera.PiCamera() as camera:
+                    camera.resolution = (WIDTH, HEIGHT)
+                    camera.framerate = FRAMERATE
+                    camera.vflip = VFLIP # flips image rightside up, as needed
+                    camera.hflip = HFLIP # flips image left-right, as needed
+                    sleep(1) # camera warm-up time
+                    print('Initializing websockets server on port %d' % WS_PORT)
+                    WebSocketWSGIHandler.http_version = '1.1'
+                    websocket_server = make_server(
+                        '', WS_PORT,
+                        server_class=WSGIServer,
+                        handler_class=WebSocketWSGIRequestHandler,
+                        app=WebSocketWSGIApplication(handler_cls=StreamingWebSocket))
+                    websocket_server.initialize_websockets_manager()
+                    websocket_thread = Thread(target=websocket_server.serve_forever)
+                    print('Initializing HTTP server on port %d' % HTTP_PORT)
+                    http_server = StreamingHttpServer()
+                    http_thread = Thread(target=http_server.serve_forever)
+                    print('Initializing broadcast thread')
+                    output = BroadcastOutput(camera)
+                    broadcast_thread = BroadcastThread(output.converter, websocket_server)
+                    print('Starting recording')
+                    camera.start_recording(output, 'yuv')
+                    try:
+                        print('Starting websockets thread')
+                        websocket_thread.start()
+                        print('Starting HTTP server thread')
+                        http_thread.start()
+                        print('Starting broadcast thread')
+                        broadcast_thread.start()
+                        while True:
+                            camera.wait_recording(1)
+                    except KeyboardInterrupt:
+                        pass
+                    finally:
+                        print('Stopping recording')
+                        camera.stop_recording()
+                        # print('Closing audio stream')
+                        # audio_stream.terminate()
+                        print('Waiting for broadcast thread to finish')
+                        broadcast_thread.join()
+                        print('Shutting down HTTP server')
+                        http_server.shutdown()
+                        print('Shutting down websockets server')
+                        websocket_server.shutdown()
+                        print('Waiting for HTTP server thread to finish')
+                        http_thread.join()
+                        print('Waiting for websockets thread to finish')
+                        websocket_thread.join()
             else:
-                print()
+                print("Bill isn't on yet")
             stream_context = model.createStream()
+
+    
+
 
 if __name__ == '__main__':
     DEFAULT_SAMPLE_RATE = 16000
@@ -226,3 +426,4 @@ if __name__ == '__main__':
     ARGS = parser.parse_args()
     if ARGS.savewav: os.makedirs(ARGS.savewav, exist_ok=True)
     main(ARGS)
+
